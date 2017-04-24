d0cb21a8897d475c5baef7d51bfdc4d200a82dbb
kkayyoo <yokoyau159@gmail.com>
2017-04-24 17:05:10 -0400

    Adding Spotify api & services

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
 
-  constructor() { }
+export class SearchComponent {
+  searchStr:string;
+
+  constructor(private _spotifyService: SpotifyService) {
+
+  }
 
   ngOnInit() {
+  }
+
+  searchMusic() {
+      this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+          console.log(res);
+      })
   }
 
 }
