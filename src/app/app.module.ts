8b629dd27ef990cf8a118457cfdf42b52ecc31c0
kkayyoo <yokoyau159@gmail.com>
2017-05-22 16:25:42 -0400

    Adding album page detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistComponent } from './components/artist/artist.component';
+import { AlbumComponent } from './components/album/album.component';
 
 import { routing } from './app.routing';
+
 
 
 @NgModule({
@@ -19,6 +21,7 @@ import { routing } from './app.routing';
     AboutComponent,
     SearchComponent,
     ArtistComponent,
+    AlbumComponent,
   ],
   imports: [
     BrowserModule,
